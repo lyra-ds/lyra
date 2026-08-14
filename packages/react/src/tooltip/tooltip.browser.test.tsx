65075eb99ce8382426794a790a93e2276177e5e2
Francisross Soares de Oliveira <franciscpd@gmail.com>
2026-08-14 09:46:59 -0300

    test(react): reset focus between tooltip cases

--- packages/react/src/tooltip/tooltip.browser.test.tsx
+++ packages/react/src/tooltip/tooltip.browser.test.tsx
@@ -10,6 +10,7 @@ function setTheme(theme: (typeof themes)[number]): void {
   else document.documentElement.removeAttribute('data-theme');
 }
 afterEach(async () => {
+  if (document.activeElement instanceof HTMLElement) document.activeElement.blur();
   await cleanup();
   setTheme('light');
 });
